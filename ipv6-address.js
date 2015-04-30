1da43fd101af9a9e8e2f3324f510abb07490e8cc
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-30 17:58:45 +0000

    Fix remaining style errors

--- ipv6-address.js
+++ ipv6-address.js
@@ -1,13 +1,13 @@
 import ArrayLikeObjectWrapper from 'array-like-object-wrapper'
 
-function toString(arr) {
-  return Array.prototype.map.call(arr, function(ele) {
+function toString (arr) {
+  return Array.prototype.map.call(arr, function (ele) {
     return ele.toString(16)
   }).join(':')
 }
 
 class Ipv6Address extends ArrayLikeObjectWrapper {
-  constructor(iterable) {
+  constructor (iterable) {
     let arr = new Uint16Array(8)
     super(arr)
 
@@ -23,11 +23,11 @@ class Ipv6Address extends ArrayLikeObjectWrapper {
     }
   }
 
-  toString() {
+  toString () {
     return toString(this)
   }
 
-  toShortString() {
+  toShortString () {
     let longestLength = 0,
       length = 0,
       longestStart,
@@ -49,15 +49,15 @@ class Ipv6Address extends ArrayLikeObjectWrapper {
       }
     }
 
-    if (typeof(longestStart) === 'undefined') {
+    if (typeof longestStart === 'undefined') {
       return this.toString()
     }
     return toString(Array.prototype.slice.call(this, 0, longestStart)) + '::' + toString(Array.prototype.slice.call(this, longestStart + longestLength))
   }
 }
 
-function parseParts(parts) {
-  return parts.map(function(part) {
+function parseParts (parts) {
+  return parts.map(function (part) {
     if (!/^[a-f0-9]{1,4}$/.test(part)) {
       throw new Error(`Ipv6Address.parse: invalid part: '${part}'`)
     }
@@ -65,39 +65,40 @@ function parseParts(parts) {
   })
 }
 
-function collapse(...arrs) {
+function collapse (...arrs) {
   return [].concat(...arrs)
 }
 
-Ipv6Address.parse = function(str) {
-  let parts = String(str).split('::')
+Object.assign(Ipv6Address, {
+  parse (str) {
+    let parts = String(str).split('::')
 
-  if (parts.length > 2) {
-    throw new Error('Ipv6Address.parse: expected "::" to occur at most once')
-  }
-  if (parts.length === 2) {
-    parts = parts.map(function(part) {
-      return parseParts(part.split(':'))
-    })
+    if (parts.length > 2) {
+      throw new Error('Ipv6Address.parse: expected "::" to occur at most once')
+    }
+    if (parts.length === 2) {
+      parts = parts.map(function (part) {
+        return parseParts(part.split(':'))
+      })
 
-    if (parts[0].length + parts[1].length > 8) {
-      throw new Error('Ipv6Address.parse: expects ":" to occur at most 9 times if "::" occurs')
+      if (parts[0].length + parts[1].length > 8) {
+        throw new Error('Ipv6Address.parse: expects ":" to occur at most 9 times if "::" occurs')
+      }
+
+      parts.splice(1, 1, new Array(8 - parts[0].length - parts[1].length).fill(0))
+      return new this(collapse(parts))
+    }
+    parts = parts[0].split(':')
+
+    if (parts.length !== 8) {
+      throw new Error('Ipv6Address.parse: expects ":" to occur exactly 7 times')
     }
 
-    parts.splice(1, 1, new Array(8 - parts[0].length - parts[1].length).fill(0))
-    return new this(collapse(parts))
+    return new this(parseParts(parts))
+  },
+  from (arrLikeOrIterable) {
+    return new this(Array.from(arrLikeOrIterable))
   }
-  parts = parts[0].split(':')
-
-  if (parts.length !== 8) {
-    throw new Error('Ipv6Address.parse: expects ":" to occur exactly 7 times')
-  }
-
-  return new this(parseParts(parts))
-}
-
-Ipv6Address.from = function(arrLikeOrIterable) {
-  return new this(Array.from(arrLikeOrIterable))
-}
+})
 
 export default Ipv6Address
